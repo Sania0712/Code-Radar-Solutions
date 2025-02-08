897ec5e955a950aff4762502e3a2e5cd9a95ca2b
Sania0712 <saniasharma0712@gmail.com>
2025-02-08 21:28:15 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,20 +4,20 @@ int main(){
     printf("");
     scanf("%d%d",&n1,&n2);
 
-    if(n1+n2){
+    if('+'){
         printf("%d",n1+n2);
     }
 
-    if(n1-n2){
+    if('-'){
         printf("%d",n1-n2);
     }
 
-    if(n1*n2){
+    if('*'){
         printf("%d",n1*n2);
     }
 
-    if(n1\n2){
-        printf("%d",n1\n2);
+    if('/'){
+        printf("%d",n1/n2);
     }
 
 
