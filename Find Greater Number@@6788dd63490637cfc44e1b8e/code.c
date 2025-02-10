b908772dcddca2d7781492c3911839b82d99e66c
Sania0712 <saniasharma0712@gmail.com>
2025-02-10 16:47:30 +0530

    Solution to Find Greater Number

--- Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(){
+
     int n1,n2;
     printf("");
     scanf("%d%d",&n1,&n2);
-    
 
-    if(n1>n2){
+    if(n1>=n2){
         printf("%d",n1);
     }
-    if(n2>n1){
+    else{
         printf("%d",n2);
     }
     return 0;
